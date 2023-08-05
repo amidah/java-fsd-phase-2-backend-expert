3b836465e109a9ac6597672b6676fcbcf121a70c
amdah <amit.dahiya.j@gmail.com>
2023-08-05 16:22:39 +0000

    hibernatge annotation example adde

---
+++ HibernateAnnotationExample/src/main/java/com/simplilearn/hibernate/annotation/dao/EmployeeDao.java
@@ -0,0 +1,46 @@
+package com.simplilearn.hibernate.annotation.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.simplilearn.hibernate.annotation.persistent.Employee;
+
+
+public class EmployeeDao {
+	
+	public static void main(String[] args) {
+
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+		SessionFactory sf = meta.getSessionFactoryBuilder().build();
+
+		Session session = sf.openSession();
+
+		Transaction t = session.beginTransaction();
+
+		Employee employee = new Employee();
+
+		employee.setId(1);
+		employee.setName("Shawn");
+		employee.setAge(28);
+		employee.setDepartment("IT");
+
+		session.save(employee);
+
+		t.commit();
+
+		System.out.println("Successfully saved...");
+		
+		sf.close();
+		session.close();
+
+	}
+
+}
---
+++ HibernateAnnotationExample/src/main/java/com/simplilearn/hibernate/annotation/persistent/Employee.java
@@ -0,0 +1,62 @@
+package com.simplilearn.hibernate.annotation.persistent;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="EMPLOYEE")
+public class Employee {
+	
+	@Id
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="AGE")
+	private int age;
+	
+	@Column(name="DEPARTMENT")
+	private String department;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", department=" + department + "]";
+	}
+
+}
